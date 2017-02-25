cf3cad49ca20b8535957a6f58e2449428a895cab
Joseph Martinsen <joseph@martinsen.com>
2017-02-24 22:24:22 -0600

    A3P1 Done except for count
    Need to ask what this is supposed to do and how they expect it to be implemented

--- A3P1/221-A3-17a-code/TemplateDoublyLinkedList/TemplateMain.cpp
+++ A3P1/221-A3-17a-code/TemplateDoublyLinkedList/TemplateMain.cpp
@@ -12,8 +12,8 @@ int main () {
   cout << "Create a new list" << endl;
   DoublyLinkedList<string> dll;
   cout << "list: " << dll << endl << endl;
-
-  // Insert 10 nodes at back with value 10,20,30,..,100
+  //
+  // // Insert 10 nodes at back with value 10,20,30,..,100
   cout << "Insert 10 nodes at back with value 10,20,30,..,100" << endl;
   for (int i=10; i<=100; i+=10) {
     stringstream ss;
@@ -30,36 +30,36 @@ int main () {
     dll.insertFirst(ss.str());
   }
   cout << "list: " << dll << endl << endl;
-  
+
   // Copy to a new list
   cout << "Copy to a new list" << endl;
   DoublyLinkedList<string> dll2(dll);
   cout << "list2: " << dll2 << endl << endl;
-  
+
   // Assign to another new list
   cout << "Assign to another new list" << endl;
   DoublyLinkedList<string> dll3;
   dll3=dll;
   cout << "list3: " << dll3 << endl << endl;
-  
+
   // Delete the last 10 nodes
-  cout << "Delete the last 10 nodes" << endl;
-  for (int i=0; i<10; i++) {
+  cout << "Delete the last 5 nodes" << endl;
+  for (int i=0; i<5; i++) {
     dll.removeLast();
   }
   cout << "list: " << dll << endl << endl;
-  
+
   // Delete the first 10 nodes
-  cout << "Delete the first 10 nodes" << endl;
-  for (int i=0; i<10; ++) {
+  cout << "Delete the first 5 nodes" << endl;
+  for (int i=0; i<5; ++i) {
     dll.removeFirst();
   }
   cout << "list: " << dll << endl << endl;
-  
+
   // Check the other two lists
   cout << "Make sure the other two lists are not affected." << endl;
   cout << "list2: " << dll2 << endl;
   cout << "list3: " << dll3 << endl;
-  
+
   return 0;
 }
